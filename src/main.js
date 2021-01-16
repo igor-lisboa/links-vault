f798e96aa4706d99341b8f7b710917148784fc41
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2021-01-16 16:03:06 -0300

    Usando store do vuex p manter dados entre componentes

--- src/main.js
+++ src/main.js
@@ -6,6 +6,15 @@ Vue.config.productionTip = false
 
 Vue.use(Vuex)
 
+const store = new Vuex.Store({
+  state: {
+    user: {
+      token: null
+    }
+  }
+})
+
 new Vue({
   render: h => h(App),
+  store
 }).$mount('#app')

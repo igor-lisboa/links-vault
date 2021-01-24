e4c73f2be8833666374dce62b30d9b1d388ebe73
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2021-01-24 14:06:48 -0300

    Configurando storage p usar localstorage ao inves de window session

--- src/store/index.js
+++ src/store/index.js
@@ -35,28 +35,10 @@ const getters = {
     loggedUser: state => Boolean(state?.user?.token)
 }
 
-const cookieEnabled = navigator.cookieEnabled
-
-if (cookieEnabled) {
-    vuexLocalStorage = new VuexPersist({
-        key: 'vuex',
-        storage: window.sessionStorage,
-        reducer: state => ({
-            user: state.user
-        })
-    })
-} else {
-    vuexLocalStorage = new VuexPersist({
-        key: 'vuex', // The key to store the state on in the storage provider.
-        storage: window.localStorage, // or window.sessionStorage or localForage
-        // Function that passes the state and returns the state with only the objects you want to store.
-        // reducer: state => state,
-        // Function that passes a mutation and lets you decide if it should update the state in localStorage.
-        // filter: mutation => (true)
-    })
-
-
-}
+vuexLocalStorage = new VuexPersist({
+    key: 'vuex', // The key to store the state on in the storage provider.
+    storage: window.localStorage, // or window.sessionStorage or localForage
+})
 
 vuexStore = new Vuex.Store({
     plugins: [vuexLocalStorage.plugin],

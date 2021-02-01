e7d7aadd9372334c289a0d6cb00079bf72d4c623
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2021-02-01 12:23:26 -0300

    Inserindo o pwa novamente

--- src/main.js
+++ src/main.js
@@ -8,12 +8,13 @@ import { BootstrapVue } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import VueSweetalert2 from 'vue-sweetalert2'
+import './registerServiceWorker'
 
 Vue.use(BootstrapVue)
 
 Vue.use(VueSweetalert2);
 
-Vue.config.productionTip = process.env.NODE_ENV !== 'production'
+Vue.config.productionTip = process.env.NODE_ENV === 'production'
 
 new Vue({
   router,
